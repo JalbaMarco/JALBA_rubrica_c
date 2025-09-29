756a899dc4ed4c4633913d1380828f2e6157c551
Marco <jalbamarco07@gmail.com>
2025-09-29 20:10:46 +0200

    Quinto commit : Main

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ creazione file e configurazione README
 ## Secondo commit
 struct del contatto con inizializzato nome,cognome e telefono
 ## terzo commit 
-funzione Aggiungi contatto dove faccio scorrere prima l'array della rubrica per vedere se c'è posto per aggiungere un conttatto, questo lo faccio con strcmp che mi confronta se in quella posizione per i tre parametri c'è spazio e se c'è si aggiunge il contatto altrimenti no
+funzione Aggiungi contatto dove faccio scorrere prima l'array della rubrica per vedere se c'è posto per aggiungere un contatto, questo lo faccio con strcmp che mi confronta se in quella posizione per i tre parametri c'è spazio e se c'è si aggiunge il contatto altrimenti no
 ## quarto commit
-funzione Visualizza contatti: in questa funzione con un for faccio scorrere tutta la rubrica e nel caso non fosse vuoto stampa il contattto 
+funzione Visualizza contatti: in questa funzione con un for faccio scorrere tutta la rubrica e nel caso non fosse vuoto stampa il contatto 
+## quinto commit
+inizializzato il main dove c'è un ciclo while che permette di sceglire in input cosa eseguire
--- main.c
+++ main.c
@@ -6,8 +6,41 @@ typedef struct {
     char telefono[15];
 
 }Contatto;
-
+int Aggiungi(Contatto a[]);
+int Visualizza(Contatto a[]);
 int main(void) {
+    Contatto rubri[20];
+    int scelta = 0;
+
+
+    for (int i = 0; i < 20; i++) {
+        strcpy(rubri[i].nome, "");
+        strcpy(rubri[i].cognome, "");
+        strcpy(rubri[i].telefono, "");  // strcpy incolla in questo caso in rubri in quella posizione la ""
+    }
+
+    while (scelta != 5) {
+        printf("\n RUBRICA \n");
+        printf("1. Aggiungi contatto\n");
+        printf("2. Visualizza rubrica\n");
+        printf("3. Ricerca per cognome \n");
+        printf("4. Elimina contatto \n");
+        printf("5. Esci\n");
+        printf("Scelta: ");
+        scanf("%d", &scelta);
+
+        if (scelta == 1) {
+            Aggiungi(rubri);
+        } else if (scelta == 2) {
+            Visualizza(rubri);
+        } else if (scelta == 3) {
+            printf("Lavori in corso\n");
+        } else if (scelta == 4) {
+            printf("Lavori in corso\n");
+        }
+    }
+        printf("Uscita dal programma...\n");
+        return 0;
 
     return 0;
 }
@@ -32,7 +65,7 @@ int Aggiungi(Contatto a[]) {
 
 
 }
-int Cerca(Contatto a[]) {
+int Visualizza(Contatto a[]) {
     for ( int i = 0; i<20; i++) {
         if (a[i].nome!="\0")
         printf("Nome: %s", a[i].nome);
