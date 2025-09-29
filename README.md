354d9468279d2f1d2bfa435bf5b6c5909112d9be
Marco <jalbamarco07@gmail.com>
2025-09-29 19:33:32 +0200

    Terzo commit : Funzione Aggiungi

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 creazione file e configurazione README
 ## Secondo commit
 struct del contatto con inizializzato nome,cognome e telefono
+## terzo commit 
+funzione Aggiungi contatto dove faccio scorrere prima l'array della rubrica per vedere se c'è posto per aggiungere un conttatto, questo lo faccio con strcmp che mi confronta se in quella posizione per i tre parametri c'è spazio e se c'è si aggiunge il contatto altrimenti no
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 typedef struct {
     char nome[30];
     char cognome[30];
@@ -9,4 +10,25 @@ typedef struct {
 int main(void) {
 
     return 0;
+}
+int Aggiungi(Contatto a[]) {
+    int n=-1;
+    for (int i = 0; i<20; i++) {
+        if ((strcmp(a[i].nome, "-\0") == 0) && (strcmp(a[i].cognome, "-\0") == 0 )&&
+        strcmp(a[i].telefono, "-\0") == 0) {
+
+           n= i;
+            break;
+            }
+    }
+    if (n == -1) {
+        printf("Rubrica piena \n");
+    }else {
+        scanf("%s", a[n].nome);
+        scanf("%s", a[n].cognome);
+        scanf("%s", a[n].telefono);
+
+    }
+
+
 }
